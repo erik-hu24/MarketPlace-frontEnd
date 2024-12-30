1159040c5cab04bd4f1fb9d3ebb6976b20b7d9ea
Larry0913 <lilingjie0913@gmail.com>
2024-12-30 04:30:34 -0500

    implement product details pageunlogin redirect to login page

--- src/components/Login_page.jsx
+++ src/components/Login_page.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useContext } from "react";
 import axios from "axios";
-import { useNavigate } from "react-router-dom";
+import { useNavigate, useLocation} from "react-router-dom";
 import "../styles/login-page.css";
 import { AuthContext } from "../AuthContext"; // 引入 AuthContext
 
@@ -13,6 +13,12 @@ const LoginPage = () => {
   const [errorMessage, setErrorMessage] = useState("");
   const { isLoggedIn, setIsLoggedIn, loggedInUser, setLoggedInUser } = useContext(AuthContext); // 使用 AuthContext
   const navigate = useNavigate();
+  const location = useLocation();
+  const searchParams = new URLSearchParams(location.search);
+  const from = searchParams.get('from') || '/';
+  console.log(searchParams);
+  console.log(location);
+  console.log('Redirecting back to:', from);
 
   const handleInputChange = (e) => {
     const { name, value } = e.target;
@@ -21,30 +27,31 @@ const LoginPage = () => {
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-
+  
     try {
       // 发送登录请求到后端
       const response = await axios.post(
         "http://localhost:3000/users/login",
         formData
       );
-
+  
       // 保存 JWT 到 localStorage
       localStorage.setItem("token", response.data.token);
       console.log("Log in Successfully!");
-
+  
       // 更新 AuthContext 的状态
       setIsLoggedIn(true);
       setLoggedInUser(response.data.user.username); // 假设后端返回用户信息中的 username
       setErrorMessage("");
-
-      // 跳转到首页
-      navigate("/");
+  
+      // 跳转回用户来源页面，默认跳转到首页
+      navigate(from);
     } catch (error) {
       setErrorMessage("Invalid email or password");
       console.error("Login failed", error);
     }
   };
+  
 
   const handleLogout = () => {
     // 清除 token 和重置状态
@@ -55,6 +62,7 @@ const LoginPage = () => {
   };
 
   return (
+    
     <div className="Hackhub-login-container">
       <div className="Hackhub-login-box">
         {!isLoggedIn ? (
